3e9203a00115421f74ac7f091455eb8f9363470c
maksim-pokhiliy <maksim.pokhiliy@gmail.com>
2024-09-24 18:39:18 +0300

    prettier and eslint added, ran lint:fix and format scripts

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 80,
+  endOfLine: "auto",
+};
